1c307c769baefde00768835b5d2547da76ccc5ac
alif.meydi <alif.meydi@rds.co.id>
2026-01-15 16:47:56 +0700

    feat; adding Document Model, and adding supabase postgresql config

---
+++ backend/TellADoc.API/TellADoc.API/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TellADoc.API.Models;
+
+namespace TellADoc.API.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public Document document { get; set; }
+    }
+}
---
+++ backend/TellADoc.API/TellADoc.API/Models/Document.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TellADoc.API.Models
+{
+    public class Document
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Type { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        [Required]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
